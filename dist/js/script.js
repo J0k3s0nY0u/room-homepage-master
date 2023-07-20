557a7c319ea2354d441cea844027afa12301ff54
J0k3s0nY0u <my.dev.vic@gmail.com>
2023-07-20 11:20:52 +0100

    modified:   dist/css/main.css 	modified:   dist/css/main.css.map 	modified:   dist/js/script.js 	modified:   index.html 	modified:   scss/_slider.scss

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,20 +9,16 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
+
+    // Auto Play
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
     },
   
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+    }
   });
