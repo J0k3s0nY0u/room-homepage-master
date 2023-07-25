05e62a2d90765923d3c79b26467be222ae140267
J0k3s0nY0u <my.dev.vic@gmail.com>
2023-07-25 15:36:45 +0100

    modified:   dist/js/script.js

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,6 +4,24 @@ const nav = document.querySelector('navbar');
 const menu = document.querySelector('navbar__menu');
 const navList = document.querySelector('navbar__list');
 
+let showMenu = false;
+
+hamburger.addEventListener('click', toggleNav);
+
+toggleNav = ()=>{
+  if(!showMenu) {
+    hamburger.classList.add('open');
+    nav.classList.add('open');
+    menu.classList.add('open');
+    navList.classList.add('open');
+  } else {
+    hamburger.classList.remove('open');
+    nav.classList.remove('open');
+    menu.classList.remove('open');
+    navList.classList.remove('open');
+  }
+};
+
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
