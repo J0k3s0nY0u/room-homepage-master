5c168ad6582c099bd56b6bc73a399d0defe3a5ee
J0k3s0nY0u <my.dev.vic@gmail.com>
2023-07-25 15:24:51 +0100

    modified:   dist/js/script.js 	modified:   index.html

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,9 @@
 
 const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('navbar');
+const menu = document.querySelector('navbar__menu');
+const navList = document.querySelector('navbar__list');
+
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
