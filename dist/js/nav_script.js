760e6cab84919d4a874fb7d9a43e89a8d3d6e055
J0k3s0nY0u <my.dev.vic@gmail.com>
2023-07-26 13:50:50 +0100

    modified:   dist/css/main.css 	modified:   dist/css/main.css.map 	modified:   dist/js/nav_script.js 	modified:   index.html 	modified:   scss/_responsive.scss

--- dist/js/nav_script.js
+++ dist/js/nav_script.js
@@ -4,6 +4,10 @@ const hamburger = document.querySelector(".hamburger");
 hamburger.onclick = ()=>{
 const closeBtn = document.querySelector(".close");
 const navBar = document.querySelector('.navbar');
+const navBarMenu = document.querySelector('.navbar__menu');
 
+hamburger.classList.toggle('open');
 navBar.classList.toggle('open');
+closeBtn.classList.toggle('open');
+navBarMenu.classList.toggle('open');
 };
