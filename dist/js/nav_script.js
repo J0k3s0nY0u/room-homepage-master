e4a83146bf5b8551707f67d87886ed555e43135f
J0k3s0nY0u <my.dev.vic@gmail.com>
2023-07-26 10:56:14 +0100

    modified:   dist/css/main.css 	modified:   dist/css/main.css.map 	modified:   dist/js/nav_script.js 	modified:   scss/_navbar.scss 	modified:   scss/_responsive.scss 	modified:   scss/_slider.scss

--- dist/js/nav_script.js
+++ dist/js/nav_script.js
@@ -1,24 +1,9 @@
 
 const hamburger = document.querySelector(".hamburger");
+
+hamburger.onclick = ()=>{
 const closeBtn = document.querySelector(".close");
 const navBar = document.querySelector('.navbar');
-const menu = document.querySelector('.navbar__menu');
-const navList = document.querySelectorAll('.navbar__list');
 
-let showMenu = true;
-
-hamburger.onclick = toggleNav = ()=>{
-  if(showMenu) {
-    hamburger.classList.add('open');
-    closeBtn.classList.add('open');
-    navBar.classList.add('open');
-    menu.classList.add('open');
-    navList.classList.add('open');
-  } else {
-    hamburger.classList.remove('open');
-    closeBtn.classList.remove('open');
-    navBar.classList.remove('open');
-    menu.classList.remove('open');
-    navList.classList.remove('open');
-  }
+navBar.classList.toggle('open');
 };
